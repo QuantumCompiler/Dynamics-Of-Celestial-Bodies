73ecf489f5ffacd49386b004a3af039f3e9de753
RelativiBit <tjlarrechea@gmail.com>
2023-11-17 23:33:31 -0800

    RK4 Update 11/17/23

--- Source/Runge Kutta 4/1stOrderExample.py
+++ Source/Runge Kutta 4/1stOrderExample.py
@@ -1,4 +1,4 @@
-import RK4 as rk4
+from RK4 import *
 import matplotlib.pyplot as plt
 
 # Initial conditions and parameters
@@ -8,13 +8,12 @@ tn = 100.0
 h = 0.1
 
 def Sine(y,t):
-    return rk4.np.sin(t)
+    return np.sin(t)
 
-y_values, t_values = rk4.RK41st(Sine, y0, t0, tn, h)
+y_values, t_values = RK41st(Sine, y0, t0, tn, h)
 
 # Plot the results
 plt.plot(t_values, y_values, label='RK4 Solution')
-# plt.plot(t_values, -rk4.np.cos(t_values) + 1, label='Analytical Solution')
 plt.xlabel('Time (t)')
 plt.ylabel('y(t)')
 plt.legend()
--- Source/Runge Kutta 4/RK4.py
+++ Source/Runge Kutta 4/RK4.py
@@ -27,7 +27,6 @@ def RK41st(ODE, a0, b0, bn, h):
     a_values = np.zeros(n + 1)
     a_values[0] = a0
     b_values = np.linspace(b0, bn, n + 1)
-
     for i in range(n):
         a = a_values[i]
         b = b_values[i]
@@ -36,5 +35,52 @@ def RK41st(ODE, a0, b0, bn, h):
         k3 = h * ODE(a + 0.5 * k2, b + 0.5 * h)
         k4 = h * ODE(a + k3, b + h)
         a_values[i + 1] = a + (1/6) * (k1 + 2 * k2 + 2 * k3 + k4)
-
     return a_values, b_values
+
+# Define the system of ODEs
+# u' = v
+# v' = f(t, u, v)
+def system_of_odes(t, u, v):
+    return v, -9.81  # Example: free-fall under gravity
+
+# RK42nd - Runge Kutta 4 ODE Solver For A Second Order ODE (Of the form d^2a / dc^2)
+# Input:
+#   ODE - Function that represents a second order ODE
+#       * Particularly, this function must return da_dc, db_dc
+#   a0 - Initial value for a
+#   b0 - Initial value for b
+#   c0 - Initial value for c
+#   cn - Final value for c
+#   h - Step Size
+# Algorithm:
+#   * Calculate the number of points n
+#   * Create lists for a, b, and c values
+#   * Initialize the values for a and b in their lists
+#   * Iterate over the total number of points n
+#       * Assign the values for a, b, and c with the current list value
+#       * Calculate the values for k1_a, k2_a, k3_a, and k4_a
+#       * Calculate the values for k1_b, k2_b, k3_b, and k4_b
+#       * Assign the next values for a and b in their lists
+#   * Return the lists
+# Output:
+#   a_values - List of a values that were found from RK4 method
+#   b_values - List of b values that were found from RK4 method
+#   c_values - List of c values that were found from RK4 method
+def RK42nd(ODE, a0, b0, c0, cn, h):
+    n = int((cn - c0) / h)
+    a_values = np.zeros(n + 1)
+    b_values = np.zeros(n + 1)
+    c_values = np.linspace(c0, cn, n + 1)
+    a_values[0] = a0
+    b_values[0] = b0
+    for i in range(n):
+        a = a_values[i]
+        b = b_values[i]
+        c = c_values[i]
+        k1_a, k1_b = h * np.array(ODE(c, a, b))
+        k2_a, k2_b = h * np.array(ODE(c + 0.5 * h, a + 0.5 * k1_a, b + 0.5 * k1_b))
+        k3_a, k3_b = h * np.array(ODE(c + 0.5 * h, a + 0.5 * k2_a, b + 0.5 * k2_b))
+        k4_a, k4_b = h * np.array(ODE(c + h, a + k3_a, b + k3_b))
+        a_values[i + 1] = a + (1/6) * (k1_a + 2 * k2_a + 2 * k3_a + k4_a)
+        b_values[i + 1] = b + (1/6) * (k1_b + 2 * k2_b + 2 * k3_b + k4_b)
+    return a_values, b_values, c_values
