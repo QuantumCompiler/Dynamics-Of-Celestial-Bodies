095cbcdaaa97a2ed61f5bccbe29ce1635b806545
RelativiBit <tjlarrechea@gmail.com>
2023-11-20 12:08:33 -0800

    RK4 Update 11/20/23

--- Source/Celestial Bodies/RK4.py
+++ Source/Celestial Bodies/RK4.py
@@ -14,36 +14,36 @@ from Modules import *
 #   * Set the initial value of a to be a0
 #   * Generate a list for b values from b0 to bn with n + 1 entries
 #   * Iterate over the total number of entries n
-#       * Update a to be the current value of a in a_values
-#       * Update b to be the current value of b in b_values
+#       * Update a to be the current value of a in avalues
+#       * Update b to be the current value of b in bvalues
 #       * Calculate k1, k2, k3, k4 from the definition of RK4
-#       * Update the next entry of a_values with the definition of RK4
-#   * Return the lists b_values and a_values
+#       * Update the next entry of avalues with the definition of RK4
+#   * Return the lists bvalues and avalues
 # Output:
-#   a_values - List of a values that were found from RK4 method
-#   b_values - List of b values that were found from RK4 method
+#   avalues - List of a values that were found from RK4 method
+#   bvalues - List of b values that were found from RK4 method
 def RK41st(ODE, a0, b0, bn, h):
     # Number of points
     n = int((bn - b0) / h)
     # List for a values
-    a_values = np.zeros(n + 1)
+    avalues = np.zeros(n + 1)
     # Initialize a
-    a_values[0] = a0
+    avalues[0] = a0
     # Initialize b
-    b_values = np.linspace(b0, bn, n + 1)
+    bvalues = np.linspace(b0, bn, n + 1)
     # Iterate over points
     for i in range(n):
         # Current state of values
-        a = a_values[i]
-        b = b_values[i]
+        a = avalues[i]
+        b = bvalues[i]
         # RK4 update
         k1 = h * ODE(a, b)
         k2 = h * ODE(a + 0.5 * k1, b + 0.5 * h)
         k3 = h * ODE(a + 0.5 * k2, b + 0.5 * h)
         k4 = h * ODE(a + k3, b + h)
         # Next state values
-        a_values[i + 1] = a + (1/6) * (k1 + 2 * k2 + 2 * k3 + k4)
-    return a_values, b_values
+        avalues[i + 1] = a + (1/6) * (k1 + 2 * k2 + 2 * k3 + k4)
+    return avalues, bvalues
 
 # RK42nd - Runge Kutta 4 ODE Solver For A Second Order ODE (Of the form d^2a / dc^2)
 # Input:
@@ -60,42 +60,42 @@ def RK41st(ODE, a0, b0, bn, h):
 #   * Initialize the values for a and b in their lists
 #   * Iterate over the total number of points n
 #       * Assign the values for a, b, and c with the current list value
-#       * Calculate the values for k1_a, k2_a, k3_a, and k4_a
-#       * Calculate the values for k1_b, k2_b, k3_b, and k4_b
+#       * Calculate the values for k1a, k2a, k3a, and k4a
+#       * Calculate the values for k1b, k2b, k3b, and k4b
 #       * Assign the next values for a and b in their lists
 #   * Return the lists
 # Output:
-#   a_values - List of a values that were found from RK4 method
-#   b_values - List of b values that were found from RK4 method
-#   c_values - List of c values that were found from RK4 method
+#   avalues - List of a values that were found from RK4 method
+#   bvalues - List of b values that were found from RK4 method
+#   cvalues - List of c values that were found from RK4 method
 def RK42nd(ODE, a0, b0, c0, cn, h):
     # Number of points
     n = int((cn - c0) / h)
     # List for a values
-    a_values = np.zeros(n + 1)
+    avalues = np.zeros(n + 1)
     # List for b values
-    b_values = np.zeros(n + 1)
+    bvalues = np.zeros(n + 1)
     # Initialize a
-    a_values[0] = a0
+    avalues[0] = a0
     # Initialize b
-    b_values[0] = b0
+    bvalues[0] = b0
     # Initialize c list
-    c_values = np.linspace(c0, cn, n + 1)
+    cvalues = np.linspace(c0, cn, n + 1)
     # Iterate over points
     for i in range(n):
         # Current state of values
-        a = a_values[i]
-        b = b_values[i]
-        c = c_values[i]
+        a = avalues[i]
+        b = bvalues[i]
+        c = cvalues[i]
         # RK4 update
-        k1_a, k1_b = h * np.array(ODE(c, a, b))
-        k2_a, k2_b = h * np.array(ODE(c + 0.5 * h, a + 0.5 * k1_a, b + 0.5 * k1_b))
-        k3_a, k3_b = h * np.array(ODE(c + 0.5 * h, a + 0.5 * k2_a, b + 0.5 * k2_b))
-        k4_a, k4_b = h * np.array(ODE(c + h, a + k3_a, b + k3_b))
+        k1a, k1b = h * np.array(ODE(c, a, b))
+        k2a, k2b = h * np.array(ODE(c + 0.5 * h, a + 0.5 * k1a, b + 0.5 * k1b))
+        k3a, k3b = h * np.array(ODE(c + 0.5 * h, a + 0.5 * k2a, b + 0.5 * k2b))
+        k4a, k4b = h * np.array(ODE(c + h, a + k3a, b + k3b))
         # Next state of values
-        a_values[i + 1] = a + (1/6) * (k1_a + 2 * k2_a + 2 * k3_a + k4_a)
-        b_values[i + 1] = b + (1/6) * (k1_b + 2 * k2_b + 2 * k3_b + k4_b)
-    return a_values, b_values, c_values
+        avalues[i + 1] = a + (1/6) * (k1a + 2 * k2a + 2 * k3a + k4a)
+        bvalues[i + 1] = b + (1/6) * (k1b + 2 * k2b + 2 * k3b + k4b)
+    return avalues, bvalues, cvalues
 
 # RK4TwoBody - RK4 Method That Solves The Force Attraction Between Two Bodies In Space
 # Input:
@@ -122,53 +122,53 @@ def RK42nd(ODE, a0, b0, c0, cn, h):
 #   mass2Pos - List of mass 2 positions with respect to time
 #   mass1Vel - List of mass 1 velocities with respect to time
 #   mass2Vel - List of mass 2 velocities with respect to time
-#   time_vals - List of times at current steps in model
+#   timevals - List of times at current steps in model
 def RK4TwoBody(ODE, massList, ic, t0, tn, h):
     # Number of points
     n = int((tn - t0) / h)
     # Mass 1 position lists
-    m1_x_vals = np.zeros(n + 1)
-    m1_y_vals = np.zeros(n + 1)
-    m1_z_vals = np.zeros(n + 1)
+    m1xvals = np.zeros(n + 1)
+    m1yvals = np.zeros(n + 1)
+    m1zvals = np.zeros(n + 1)
     # Mass 2 position lists
-    m2_x_vals = np.zeros(n + 1)
-    m2_y_vals = np.zeros(n + 1)
-    m2_z_vals = np.zeros(n + 1)
+    m2xvals = np.zeros(n + 1)
+    m2yvals = np.zeros(n + 1)
+    m2zvals = np.zeros(n + 1)
     # Mass 1 velocity lists
-    m1_vx_vals = np.zeros(n + 1)
-    m1_vy_vals = np.zeros(n + 1)
-    m1_vz_vals = np.zeros(n + 1)
+    m1vxvals = np.zeros(n + 1)
+    m1vyvals = np.zeros(n + 1)
+    m1vzvals = np.zeros(n + 1)
     # Mass 2 velocity lists
-    m2_vx_vals = np.zeros(n + 1)
-    m2_vy_vals = np.zeros(n + 1)
-    m2_vz_vals = np.zeros(n + 1)
+    m2vxvals = np.zeros(n + 1)
+    m2vyvals = np.zeros(n + 1)
+    m2vzvals = np.zeros(n + 1)
     # Initializing mass 1 position lists
-    m1_x_vals[0] = ic[0][0]
-    m1_y_vals[0] = ic[0][1]
-    m1_z_vals[0] = ic[0][2]
+    m1xvals[0] = ic[0][0]
+    m1yvals[0] = ic[0][1]
+    m1zvals[0] = ic[0][2]
     # Initializing mass 2 position lists
-    m2_x_vals[0] = ic[1][0]
-    m2_y_vals[0] = ic[1][1]
-    m2_z_vals[0] = ic[1][2]
+    m2xvals[0] = ic[1][0]
+    m2yvals[0] = ic[1][1]
+    m2zvals[0] = ic[1][2]
     # Initializing mass 1 velocity lists
-    m1_vx_vals[0] = ic[2][0]
-    m1_vy_vals[0] = ic[2][1]
-    m1_vz_vals[0] = ic[2][2]
+    m1vxvals[0] = ic[2][0]
+    m1vyvals[0] = ic[2][1]
+    m1vzvals[0] = ic[2][2]
     # Initializing mass 2 velocity lists
-    m2_vx_vals[0] = ic[3][0]
-    m2_vy_vals[0] = ic[3][1]
-    m2_vz_vals[0] = ic[3][2]
+    m2vxvals[0] = ic[3][0]
+    m2vyvals[0] = ic[3][1]
+    m2vzvals[0] = ic[3][2]
     # Initialize time list
-    time_vals = np.linspace(t0, tn, n + 1)
+    timevals = np.linspace(t0, tn, n + 1)
     # Iterate over points
     for i in range(n):
         # Current state of system
-        state = (m1_x_vals[i], m1_y_vals[i], m1_z_vals[i], 
-                m2_x_vals[i], m2_y_vals[i], m2_z_vals[i], 
-                m1_vx_vals[i], m1_vy_vals[i], m1_vz_vals[i], 
-                m2_vx_vals[i], m2_vy_vals[i], m2_vz_vals[i])
+        state = (m1xvals[i], m1yvals[i], m1zvals[i], 
+                m2xvals[i], m2yvals[i], m2zvals[i], 
+                m1vxvals[i], m1vyvals[i], m1vzvals[i], 
+                m2vxvals[i], m2vyvals[i], m2vzvals[i])
         # Current time of state
-        t = time_vals[i]
+        t = timevals[i]
         # RK4 update
         k1 = np.array(ODE(t, massList, *state))
         k2 = np.array(ODE(t + 0.5 * h, massList, *(np.add(state, 0.5 * h * k1))))
@@ -177,16 +177,16 @@ def RK4TwoBody(ODE, massList, ic, t0, tn, h):
         # Next state of system
         next_state = np.add(state, (h / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4))
         # Update next stat of system
-        (m1_x_vals[i + 1], m1_y_vals[i + 1], m1_z_vals[i + 1],
-        m2_x_vals[i + 1], m2_y_vals[i + 1], m2_z_vals[i + 1],
-        m1_vx_vals[i + 1], m1_vy_vals[i + 1], m1_vz_vals[i + 1],
-        m2_vx_vals[i + 1], m2_vy_vals[i + 1], m2_vz_vals[i + 1]) = next_state
+        (m1xvals[i + 1], m1yvals[i + 1], m1zvals[i + 1],
+        m2xvals[i + 1], m2yvals[i + 1], m2zvals[i + 1],
+        m1vxvals[i + 1], m1vyvals[i + 1], m1vzvals[i + 1],
+        m2vxvals[i + 1], m2vyvals[i + 1], m2vzvals[i + 1]) = next_state
     # Parameter lists
-    mass1Pos = [m1_x_vals, m1_y_vals, m1_z_vals]
-    mass1Vel = [m1_vx_vals, m1_vy_vals, m1_vz_vals]
-    mass2Pos = [m2_x_vals, m2_y_vals, m2_z_vals]
-    mass2Vel = [m2_vx_vals, m2_vy_vals, m2_vz_vals]
-    return mass1Pos, mass2Pos, mass1Vel, mass2Vel, time_vals
+    mass1Pos = [m1xvals, m1yvals, m1zvals]
+    mass1Vel = [m1vxvals, m1vyvals, m1vzvals]
+    mass2Pos = [m2xvals, m2yvals, m2zvals]
+    mass2Vel = [m2vxvals, m2vyvals, m2vzvals]
+    return mass1Pos, mass2Pos, mass1Vel, mass2Vel, timevals
 
 # RK4ThreeBody - RK4 Method That Solves The Force Attraction Between Three Bodies In Space
 # Input:
@@ -215,71 +215,71 @@ def RK4TwoBody(ODE, massList, ic, t0, tn, h):
 #   mass1Vel - List of mass 1 velocities with respect to time
 #   mass2Vel - List of mass 2 velocities with respect to time
 #   mass3Vel - List of mass 3 velocities with respect to time
-#   time_vals - List of times at current steps in model
+#   timevals - List of times at current steps in model
 def RK4ThreeBody(ODE, massList, ic, t0, tn, h):
     # Number of points
     n = int((tn - t0) / h)
     # Mass 1 position lists
-    m1_x_vals = np.zeros(n + 1)
-    m1_y_vals = np.zeros(n + 1)
-    m1_z_vals = np.zeros(n + 1)
+    m1xvals = np.zeros(n + 1)
+    m1yvals = np.zeros(n + 1)
+    m1zvals = np.zeros(n + 1)
     # Mass 2 position lists
-    m2_x_vals = np.zeros(n + 1)
-    m2_y_vals = np.zeros(n + 1)
-    m2_z_vals = np.zeros(n + 1)
+    m2xvals = np.zeros(n + 1)
+    m2yvals = np.zeros(n + 1)
+    m2zvals = np.zeros(n + 1)
     # Mass 3 position lists
-    m3_x_vals = np.zeros(n + 1)
-    m3_y_vals = np.zeros(n + 1)
-    m3_z_vals = np.zeros(n + 1)
+    m3xvals = np.zeros(n + 1)
+    m3yvals = np.zeros(n + 1)
+    m3zvals = np.zeros(n + 1)
     # Mass 1 velocity lists
-    m1_vx_vals = np.zeros(n + 1)
-    m1_vy_vals = np.zeros(n + 1)
-    m1_vz_vals = np.zeros(n + 1)
+    m1vxvals = np.zeros(n + 1)
+    m1vyvals = np.zeros(n + 1)
+    m1vzvals = np.zeros(n + 1)
     # Mass 2 velocity lists
-    m2_vx_vals = np.zeros(n + 1)
-    m2_vy_vals = np.zeros(n + 1)
-    m2_vz_vals = np.zeros(n + 1)
+    m2vxvals = np.zeros(n + 1)
+    m2vyvals = np.zeros(n + 1)
+    m2vzvals = np.zeros(n + 1)
     # Mass 3 velocity lists
-    m3_vx_vals = np.zeros(n + 1)
-    m3_vy_vals = np.zeros(n + 1)
-    m3_vz_vals = np.zeros(n + 1)
+    m3vxvals = np.zeros(n + 1)
+    m3vyvals = np.zeros(n + 1)
+    m3vzvals = np.zeros(n + 1)
     # Initializing mass 1 position lists
-    m1_x_vals[0] = ic[0][0]
-    m1_y_vals[0] = ic[0][1]
-    m1_z_vals[0] = ic[0][2]
+    m1xvals[0] = ic[0][0]
+    m1yvals[0] = ic[0][1]
+    m1zvals[0] = ic[0][2]
     # Initializing mass 2 position lists
-    m2_x_vals[0] = ic[1][0]
-    m2_y_vals[0] = ic[1][1]
-    m2_z_vals[0] = ic[1][2]
+    m2xvals[0] = ic[1][0]
+    m2yvals[0] = ic[1][1]
+    m2zvals[0] = ic[1][2]
     # Initializing mass 3 position lists
-    m3_x_vals[0] = ic[2][0]
-    m3_y_vals[0] = ic[2][1]
-    m3_z_vals[0] = ic[2][2]
+    m3xvals[0] = ic[2][0]
+    m3yvals[0] = ic[2][1]
+    m3zvals[0] = ic[2][2]
     # Initializing mass 1 velocity lists
-    m1_vx_vals[0] = ic[3][0]
-    m1_vy_vals[0] = ic[3][1]
-    m1_vz_vals[0] = ic[3][2]
+    m1vxvals[0] = ic[3][0]
+    m1vyvals[0] = ic[3][1]
+    m1vzvals[0] = ic[3][2]
     # Initializing mass 2 velocity lists
-    m2_vx_vals[0] = ic[4][0]
-    m2_vy_vals[0] = ic[4][1]
-    m2_vz_vals[0] = ic[4][2]
+    m2vxvals[0] = ic[4][0]
+    m2vyvals[0] = ic[4][1]
+    m2vzvals[0] = ic[4][2]
     # Initializing mass 3 velocity lists
-    m3_vx_vals[0] = ic[5][0]
-    m3_vy_vals[0] = ic[5][1]
-    m3_vz_vals[0] = ic[5][2]
+    m3vxvals[0] = ic[5][0]
+    m3vyvals[0] = ic[5][1]
+    m3vzvals[0] = ic[5][2]
     # Initialize time list
-    time_vals = np.linspace(t0, tn, n + 1)
+    timevals = np.linspace(t0, tn, n + 1)
     # Iterate over points
     for i in range(n):
         # Current state of system
-        state = (m1_x_vals[i], m1_y_vals[i], m1_z_vals[i],
-                m2_x_vals[i], m2_y_vals[i], m2_z_vals[i],
-                m3_x_vals[i], m3_y_vals[i], m3_z_vals[i],
-                m1_vx_vals[i], m1_vy_vals[i], m1_vz_vals[i],
-                m2_vx_vals[i], m2_vy_vals[i], m2_vz_vals[i],
-                m3_vx_vals[i], m3_vy_vals[i], m3_vz_vals[i])
+        state = (m1xvals[i], m1yvals[i], m1zvals[i],
+                m2xvals[i], m2yvals[i], m2zvals[i],
+                m3xvals[i], m3yvals[i], m3zvals[i],
+                m1vxvals[i], m1vyvals[i], m1vzvals[i],
+                m2vxvals[i], m2vyvals[i], m2vzvals[i],
+                m3vxvals[i], m3vyvals[i], m3vzvals[i])
         # Current time of state
-        t = time_vals[i]
+        t = timevals[i]
         # RK4 update
         k1 = np.array(ODE(t, massList, *state))
         k2 = np.array(ODE(t + 0.5 * h, massList, *(np.add(state, 0.5 * h * k1))))
@@ -288,17 +288,17 @@ def RK4ThreeBody(ODE, massList, ic, t0, tn, h):
         # Next state of system
         next_state = np.add(state, (h / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4))
         # Update next stat of system
-        (m1_x_vals[i + 1], m1_y_vals[i + 1], m1_z_vals[i + 1],
-        m2_x_vals[i + 1], m2_y_vals[i + 1], m2_z_vals[i + 1],
-        m3_x_vals[i + 1], m3_y_vals[i + 1], m3_z_vals[i + 1],
-        m1_vx_vals[i + 1], m1_vy_vals[i + 1], m1_vz_vals[i + 1],
-        m2_vx_vals[i + 1], m2_vy_vals[i + 1], m2_vz_vals[i + 1],
-        m3_vx_vals[i + 1], m3_vy_vals[i + 1], m3_vz_vals[i + 1]) = next_state
+        (m1xvals[i + 1], m1yvals[i + 1], m1zvals[i + 1],
+        m2xvals[i + 1], m2yvals[i + 1], m2zvals[i + 1],
+        m3xvals[i + 1], m3yvals[i + 1], m3zvals[i + 1],
+        m1vxvals[i + 1], m1vyvals[i + 1], m1vzvals[i + 1],
+        m2vxvals[i + 1], m2vyvals[i + 1], m2vzvals[i + 1],
+        m3vxvals[i + 1], m3vyvals[i + 1], m3vzvals[i + 1]) = next_state
     # Parameter lists
-    mass1Pos = [m1_x_vals, m1_y_vals, m1_z_vals]
-    mass1Vel = [m1_vx_vals, m1_vy_vals, m1_vz_vals]
-    mass2Pos = [m2_x_vals, m2_y_vals, m2_z_vals]
-    mass2Vel = [m2_vx_vals, m2_vy_vals, m2_vz_vals]
-    mass3Pos = [m3_x_vals, m3_y_vals, m3_z_vals]
-    mass3Vel = [m3_vx_vals, m3_vy_vals, m3_vz_vals]
-    return mass1Pos, mass2Pos, mass3Pos, mass1Vel, mass2Vel, mass3Vel, time_vals
+    mass1Pos = [m1xvals, m1yvals, m1zvals]
+    mass1Vel = [m1vxvals, m1vyvals, m1vzvals]
+    mass2Pos = [m2xvals, m2yvals, m2zvals]
+    mass2Vel = [m2vxvals, m2vyvals, m2vzvals]
+    mass3Pos = [m3xvals, m3yvals, m3zvals]
+    mass3Vel = [m3vxvals, m3vyvals, m3vzvals]
+    return mass1Pos, mass2Pos, mass3Pos, mass1Vel, mass2Vel, mass3Vel, timevals
