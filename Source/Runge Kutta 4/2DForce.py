df5073320c815236f5875eaa57ab9dade4fa46d1
RelativiBit <tjlarrechea@gmail.com>
2023-11-20 22:25:41 -0800

    2D Force Update 11/20/23

--- Source/Runge Kutta 4/2DForce.py
+++ Source/Runge Kutta 4/2DForce.py
@@ -8,17 +8,3 @@ def Newton(t,a,b):
     da_dt = b
     db_dt = -1 * (G*M / r**2)
     return da_dt, db_dt
-
-y0 = 100
-v0 = 0
-t0 = 0
-tn = 4.52
-h = 0.1
-
-y_values, v_values, t_values = RK42nd(Newton, y0, v0, t0, tn, h)
-
-plt.plot(t_values, y_values)
-plt.title(f"Position Versus Time W/ $y_{0} = 100$, $y'_{0} = 0$")
-plt.xlabel(f"Time In Seconds")
-plt.ylabel(f"Position In Meters")
-plt.show()
