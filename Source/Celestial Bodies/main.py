83a07b31f987b3e2535a2c17466f7436240283bb
RelativiBit <tjlarrechea@gmail.com>
2023-11-20 19:12:10 -0800

    Main Update 11/20/23

--- Source/Celestial Bodies/main.py
+++ Source/Celestial Bodies/main.py
@@ -1,26 +1,18 @@
 # Imports
 from ModelFunctions import *
 
-threeMasses = [MSUN, MEARTH, MMOON]
+class MainWindow(QMainWindow):
+    # Constructor
+    def __init__(self):
+        super().__init__()
+        # Title of Window
+        self.setWindowTitle("Celestial Bodies")
+        # Height and Width of Window
+        self.resize(850,500)
+        self.setMinimumWidth(850)
+        self.setMinimumHeight(400)
 
-threeIC = [SUNPOS, EARTHPOS, MOONPOS, SUNVEL, EARTHVEL, MOONVEL]
-
-CoupledThreeBody2DPlotPos(threeMasses, threeIC, 0, EARTHPERIOD, 0, 1, "Sun", "Earth", "Moon")
-
-CoupledThreeBody2DAnimPos(threeMasses, threeIC, 0, EARTHPERIOD, 0, 1, "Sun", "Earth", "Moon")
-
-# class MainWindow(QMainWindow):
-#     # Constructor
-#     def __init__(self):
-#         super().__init__()
-#         # Title of Window
-#         self.setWindowTitle("Celestial Bodies")
-#         # Height and Width of Window
-#         self.resize(850,500)
-#         self.setMinimumWidth(850)
-#         self.setMinimumHeight(400)
-
-# app = QApplication(sys.argv)
-# window = MainWindow()
-# window.show()
-# sys.exit(app.exec())
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec())
