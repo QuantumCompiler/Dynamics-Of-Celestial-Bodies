74bc41477cc91e8b5b97cf546def10430f598ba5
QuantumCompiler <tjlarrechea@gmail.com>
2023-12-11 21:35:51 -0800

    Windows Update 12/11/23

--- Source/Celestial Bodies/Windows.py
+++ Source/Celestial Bodies/Windows.py
@@ -1,5 +1,89 @@
 from ModelFunctions import *
 
+# Widget Names 
+HeaderLabel = "Choose A Simulation"
+TwoDForceLabel = "2D Force"
+TwoBodyLabel = "2 Body Simulation"
+ThreeBodyLabel = "3 Body Simulation"
+
+# Object Names
+TwoDForceObjName = "2D Force Button"
+TwoBodyObjName = "2 Body Button"
+ThreeBodyObjName = "3 Body Button"
+
+class MainWindow(QMainWindow):
+    # Constructor
+    def __init__(self):
+        super().__init__()
+        # Title of Window
+        self.setWindowTitle("Celestial Bodies")
+        # Height and Width of Window
+        self.resize(800,500)
+        self.setMinimumWidth(800)
+        self.setMinimumHeight(500)
+        # Header label
+        self.headerLabel = QLabel(HeaderLabel, self)
+        self.centerWidgetHoriz(self.headerLabel)
+        headerFont = self.headerLabel.font()
+        headerFont.setPointSize(50)
+        self.headerLabel.setFont(headerFont)
+        self.headerLabel.adjustSize()
+        self.headerLabel.move((self.size().width() - self.headerLabel.size().width()) // 2, int(0.05 * self.size().height()))
+        # 2 Body Button
+        self.TwoDForceBtn = QPushButton(TwoDForceLabel, self)
+        self.TwoDForceBtn.setFixedSize(400, 50)
+        self.TwoDForceBtn.setObjectName(TwoDForceObjName)
+        self.TwoDForceBtn.clicked.connect(self.OpenTwoDForceWindow)
+        self.centerWidgetHoriz(self.TwoDForceBtn)
+        self.TwoDForceBtn.move(self.TwoDForceBtn.pos().x(), 6 * self.headerLabel.pos().y())
+        # 2 Body Button
+        self.TwoBodyBtn = QPushButton(TwoBodyLabel, self)
+        self.TwoBodyBtn.setFixedSize(400, 50)
+        self.TwoBodyBtn.setObjectName(TwoBodyObjName)
+        self.TwoBodyBtn.clicked.connect(self.OpenTwoBodyWindow)
+        self.centerWidgetHoriz(self.TwoBodyBtn)
+        self.TwoBodyBtn.move(self.TwoBodyBtn.pos().x(), self.TwoDForceBtn.pos().y() + 50)
+        # 3 Body Button
+        self.ThreeBodyBtn = QPushButton(ThreeBodyLabel, self)
+        self.ThreeBodyBtn.setFixedSize(400, 50)
+        self.ThreeBodyBtn.setObjectName(ThreeBodyObjName)
+        self.ThreeBodyBtn.clicked.connect(self.OpenThreeBodyWindow)
+        self.centerWidgetHoriz(self.ThreeBodyBtn)
+        self.ThreeBodyBtn.move(self.ThreeBodyBtn.pos().x(), self.TwoBodyBtn.pos().y() + 50)
+        self.secondWindow = None
+        
+    def centerWidgetHoriz(self, widget):
+        windowWidth = self.size().width()
+        widgetWidth = widget.size().width()
+        widgetYPos = widget.pos().y()
+        horizCenter = (windowWidth - widgetWidth) // 2
+        widget.move(horizCenter, widgetYPos)
+
+    def resizeEvent(self, event):
+        self.centerWidgetHoriz(self.headerLabel)
+        self.centerWidgetHoriz(self.TwoDForceBtn)
+        self.centerWidgetHoriz(self.TwoBodyBtn)
+        self.centerWidgetHoriz(self.ThreeBodyBtn)
+        super().resizeEvent(event)
+
+    def OpenTwoDForceWindow(self):
+        if not self.secondWindow:
+            self.secondWindow = TwoDForceWindow()
+        self.secondWindow.show()
+        self.close()
+
+    def OpenTwoBodyWindow(self):
+        if not self.secondWindow:
+            self.secondWindow = TwoBodyWindow()
+        self.secondWindow.show()
+        self.close()
+
+    def OpenThreeBodyWindow(self):
+        if not self.secondWindow:
+            self.secondWindow = ThreeBodyWindow()
+        self.secondWindow.show()
+        self.close()
+
 class TwoDForceWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -10,26 +94,15 @@ class TwoDForceWindow(QWidget):
         self.setMinimumWidth(800)
         self.setMinimumHeight(500)
         # Layouts
-        mainLayout = QHBoxLayout()
-        leftColumn = QVBoxLayout()
-        rightColumn = QVBoxLayout()
-        # Left column
-        leftColumnHeader = QLabel("Parameters")
-        leftHeaderFont = leftColumnHeader.font()
-        leftHeaderFont.setPointSize(30)
-        leftColumnHeader.setFont(leftHeaderFont)
-        leftColumnHeader.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        leftColumn.addWidget(leftColumnHeader, Qt.AlignmentFlag.AlignHCenter)
-        # Right column
-        rightColumnHeader = QLabel("Output")
-        rightHeaderFont = rightColumnHeader.font()
-        rightHeaderFont.setPointSize(30)
-        rightColumnHeader.setFont(rightHeaderFont)
-        rightColumnHeader.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        rightColumn.addWidget(rightColumnHeader, Qt.AlignmentFlag.AlignHCenter)
+        mainLayout = QVBoxLayout()
+        # Center Column Header
+        centerColumnHeader = QLabel("Parameters")
+        centerColumnHeaderFont = centerColumnHeader.font()
+        centerColumnHeaderFont.setPointSize(30)
+        centerColumnHeader.setFont(centerColumnHeaderFont)
+        centerColumnHeader.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        mainLayout.addWidget(centerColumnHeader, Qt.AlignmentFlag.AlignHCenter)
         # Add layouts
-        mainLayout.addLayout(leftColumn)
-        mainLayout.addLayout(rightColumn)
         self.setLayout(mainLayout)
 
 class TwoBodyWindow(QWidget):
@@ -42,26 +115,17 @@ class TwoBodyWindow(QWidget):
         self.setMinimumWidth(800)
         self.setMinimumHeight(500)
         # Layouts
-        mainLayout = QHBoxLayout()
-        leftColumn = QVBoxLayout()
-        rightColumn = QVBoxLayout()
-        # Left column
-        leftColumnHeader = QLabel("Parameters")
-        leftHeaderFont = leftColumnHeader.font()
-        leftHeaderFont.setPointSize(30)
-        leftColumnHeader.setFont(leftHeaderFont)
-        leftColumnHeader.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        leftColumn.addWidget(leftColumnHeader, Qt.AlignmentFlag.AlignHCenter)
-        # Right column
-        rightColumnHeader = QLabel("Output")
-        rightHeaderFont = rightColumnHeader.font()
-        rightHeaderFont.setPointSize(30)
-        rightColumnHeader.setFont(rightHeaderFont)
-        rightColumnHeader.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        rightColumn.addWidget(rightColumnHeader, Qt.AlignmentFlag.AlignHCenter)
+        mainLayout = QVBoxLayout()
+        # Layouts
+        mainLayout = QVBoxLayout()
+        # Center Column Header
+        centerColumnHeader = QLabel("Parameters")
+        centerColumnHeaderFont = centerColumnHeader.font()
+        centerColumnHeaderFont.setPointSize(30)
+        centerColumnHeader.setFont(centerColumnHeaderFont)
+        centerColumnHeader.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        mainLayout.addWidget(centerColumnHeader, Qt.AlignmentFlag.AlignHCenter)
         # Add layouts
-        mainLayout.addLayout(leftColumn)
-        mainLayout.addLayout(rightColumn)
         self.setLayout(mainLayout)
 
 class ThreeBodyWindow(QWidget):
@@ -74,24 +138,21 @@ class ThreeBodyWindow(QWidget):
         self.setMinimumWidth(800)
         self.setMinimumHeight(500)
         # Layouts
-        mainLayout = QHBoxLayout()
-        leftColumn = QVBoxLayout()
-        rightColumn = QVBoxLayout()
-        # Left column
-        leftColumnHeader = QLabel("Parameters")
-        leftHeaderFont = leftColumnHeader.font()
-        leftHeaderFont.setPointSize(30)
-        leftColumnHeader.setFont(leftHeaderFont)
-        leftColumnHeader.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        leftColumn.addWidget(leftColumnHeader, Qt.AlignmentFlag.AlignHCenter)
-        # Right column
-        rightColumnHeader = QLabel("Output")
-        rightHeaderFont = rightColumnHeader.font()
-        rightHeaderFont.setPointSize(30)
-        rightColumnHeader.setFont(rightHeaderFont)
-        rightColumnHeader.setAlignment(Qt.AlignmentFlag.AlignHCenter)
-        rightColumn.addWidget(rightColumnHeader, Qt.AlignmentFlag.AlignHCenter)
+        mainLayout = QVBoxLayout()
+        # Layouts
+        mainLayout = QVBoxLayout()
+        # Center Column Header
+        centerColumnHeader = QLabel("Parameters")
+        centerColumnHeaderFont = centerColumnHeader.font()
+        centerColumnHeaderFont.setPointSize(30)
+        centerColumnHeader.setFont(centerColumnHeaderFont)
+        centerColumnHeader.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+        mainLayout.addWidget(centerColumnHeader, Qt.AlignmentFlag.AlignHCenter)
         # Add layouts
-        mainLayout.addLayout(leftColumn)
-        mainLayout.addLayout(rightColumn)
         self.setLayout(mainLayout)
+
+def RunProgram():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
