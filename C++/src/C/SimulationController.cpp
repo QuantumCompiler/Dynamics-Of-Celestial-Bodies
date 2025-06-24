e45338a1d46f5961d0cb8d075148d7e513479b32
Taylor Larrechea <tjlarrechea@gmail.com>
2025-06-23 20:57:09 -0600

    SimulationController - Comments / notes for how this works.

--- C++/src/C/SimulationController.cpp
+++ C++/src/C/SimulationController.cpp
@@ -1,6 +1,22 @@
 #include "SimulationController.h"
 #include "QDebug"
 
+/*  SimulationController - Basic C++ class to simulate a controller for a simulation.
+
+    Inheritance - QObject: This class inherits from QObject, which is the base class for all Qt objects.
+
+    Access Specifier - public: The class is publicly accessible, meaning it can be instantiated and used in other parts of the application.
+
+    Constructor:
+
+        Input Parameter:
+            QObject *parent: A pointer to the parent QObject, which is used for memory management and object hierarchy in Qt.
+        Purpose: Initializes the SimulationController object and sets its parent.
+
+        Body:
+            Q_INVOKABLE void runSimulation(): This method is declared as invokable, allowing it to be called from QML or other contexts. 
+            It will execute the simulation logic.
+*/
 SimulationController::SimulationController(QObject *parent) : QObject(parent) {}
 
 void SimulationController::runSimulation() {
--- C++/src/C/SimulationController.h
+++ C++/src/C/SimulationController.h
@@ -2,6 +2,22 @@
 #pragma once
 #include <QObject>
 
+/*  SimulationController - Basic C++ class to simulate a controller for a simulation.
+
+    Inheritance - QObject: This class inherits from QObject, which is the base class for all Qt objects.
+
+    Access Specifier - public: The class is publicly accessible, meaning it can be instantiated and used in other parts of the application.
+
+    Constructor:
+
+        Input Parameter:
+            QObject *parent: A pointer to the parent QObject, which is used for memory management and object hierarchy in Qt.
+        Purpose: Initializes the SimulationController object and sets its parent.
+
+        Body:
+            Q_INVOKABLE void runSimulation(): This method is declared as invokable, allowing it to be called from QML or other contexts. 
+            It will execute the simulation logic.
+*/
 class SimulationController : public QObject {
     Q_OBJECT
 public:
