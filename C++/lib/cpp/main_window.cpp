71cea204dc9017a16844508648bc15e27e26cf2b
Taylor Larrechea <tjlarrechea@gmail.com>
2025-06-23 20:10:11 -0600

    Clean Up and Git Ignore update

--- C++/lib/cpp/main_window.cpp
+++
@@ -1,8 +0,0 @@
-#include "../headers/main_window.h"
-
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent)
-{
-    this->setWindowTitle("Celestial Bodies");
-    this->resize(800, 500);
-    this->setMinimumSize(400, 300);
-}
