527f812a04f48e15a7c7f2800837e8cf9373f546
Taylor Larrechea <tjlarrechea@gmail.com>
2025-06-23 21:37:29 -0600

    Main CPP File - Created invokable python script for simulation.

--- C++/src/C/main.cpp
+++ C++/src/C/main.cpp
@@ -3,13 +3,16 @@
 #include <QQmlContext>
 
 #include "CPPSimulationController.h"
+#include "PythonSimulationController.h"
 
 int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
     CPPSimulationController cppSimController;
+    PythonSimulationController pySimController;
     engine.rootContext()->setContextProperty("simController", &cppSimController);
+    engine.rootContext()->setContextProperty("pySimController", &pySimController);
 
     engine.load(QUrl(QStringLiteral("qrc:src/QML/main.qml")));
     if (engine.rootObjects().isEmpty())
